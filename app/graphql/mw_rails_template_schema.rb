befcb06f29c6d5426a5ca3185ab3bf3e22d43f0f
minwoo <whalsdn9412@gmail.com>
2019-12-15 16:16:20 +0900

    Add graphql-batch; add lazy resolver

--- app/graphql/mw_rails_template_schema.rb
+++ app/graphql/mw_rails_template_schema.rb
@@ -1,4 +1,9 @@
 class MwRailsTemplateSchema < GraphQL::Schema
+
   mutation(Types::MutationType)
   query(Types::QueryType)
+
+  rescue_from(ActiveRecord::RecordNotFound) { "Not found" }
+
+  use GraphQL::Batch
 end
